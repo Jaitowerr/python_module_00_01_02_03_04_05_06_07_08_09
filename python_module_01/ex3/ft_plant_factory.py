d432c67825e3915a667b9e48ff3bad612047d301
ATM <207956068+Jaitowerr@users.noreply.github.com>
2026-03-11 21:29:36 +0100

    Add files via upload

---
+++ python_module_01/ex3/ft_plant_factory.py
@@ -0,0 +1,10 @@
+class Plant:
+
+    def __init__(self, name: str, height: int, age: int) -> None:
+        self.name = name
+        self.height = height
+        self.age_days = age
+    
+
+if __name__ == '__main__':
+    pass
