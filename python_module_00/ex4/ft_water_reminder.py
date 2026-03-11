d1faaa894e057603450842a9bee63ed035d14a59
ATM <207956068+Jaitowerr@users.noreply.github.com>
2026-03-11 21:29:13 +0100

    Delete python_module_00 directory

--- python_module_00/ex4/ft_water_reminder.py
+++
@@ -1,9 +0,0 @@
-def ft_water_reminder():
-    need_water = int(input('Days since last watering: '))
-    if need_water > 2:
-        print('Water the plants!')
-    else:
-        print('Plants are fine')
-
-# python3 -c "from ft_water_reminder import
-# ft_water_reminder; ft_water_reminder()"
