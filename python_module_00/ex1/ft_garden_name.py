a22740efc15fa1e71a251bc28970476973037a8a
ATM <207956068+Jaitowerr@users.noreply.github.com>
2026-03-23 17:47:28 +0100

    V 3.0

---
+++ python_module_00/ex1/ft_garden_name.py
@@ -0,0 +1,5 @@
+def ft_garden_name():
+    name = input('Enter garden name: ')
+    print(f'Garden: {name}')
+    print('Status: Growing well!')
+# python3 -c "from ft_garden_name import ft_garden_name; ft_garden_name()"
