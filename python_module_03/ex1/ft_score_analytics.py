528ac09d1fe2c33d99a2444dd9f6f241f75fe1a1
ATM <207956068+Jaitowerr@users.noreply.github.com>
2026-03-31 01:00:36 +0200

    V 3.0

--- python_module_03/ex1/ft_score_analytics.py
+++ python_module_03/ex1/ft_score_analytics.py
@@ -4,21 +4,12 @@ import sys
 
 def score(list_argv: list) -> None:
     score_processed = list_argv
-    total_players = 0
-    total_score = 0
-    avg_score = 0
-    high_score = 0
-    low_score = 0
-    score_range = 0
-
-    for i in list_argv:
-        # score_processed += i
-        total_players += 1
-        total_score += int(i)
-        avg_score = total_score / total_players
-        high_score = max(score_processed)
-        low_score = min(score_processed)
-        score_range = high_score - low_score
+    total_players = len(score_processed)
+    total_score = sum(score_processed)
+    avg_score = total_score / total_players
+    high_score = max(score_processed)
+    low_score = min(score_processed)
+    score_range = high_score - low_score
 
     print(f'  - Scores processed: {score_processed}')
     print(f'  - Total players: {total_players}')
@@ -26,35 +17,28 @@ def score(list_argv: list) -> None:
     print(f'  - Average score: {avg_score}')
     print(f'  - High score: {high_score}')
     print(f'  - Low score: {low_score}')
-    print(f'  - Score range:{score_range}')
+    print(f'  - Score range: {score_range}')
 
 
 def main() -> None:
-    print('=== Player Score Analytics ===')
+    print('\n==============================\n'
+          '=== Player Score Analytics ===\n'
+          '===============================\n')
     list_argv = sys.argv
-    size_list = len(sys.argv[1:])
+    size_list = len(list_argv[1:])
     score_validos = []
 
     for i in list_argv[1:]:
         try:
-            score_validos.append(int(i))
+            score_validos += [int(i)]
         except ValueError as e:
             print(f'Invalid parameter: {i}')
 
-    if size_list >= 1:
+    if len(score_validos) >= 1:
         score(score_validos)
     else:
         print(f'  *** No scores provided. Usage: python3 {sys.argv[0]} '
         '<score1> <score2> ...')
-    
-    print(size_list)
-
-
-
-
-
-
-
 
 
 if __name__ == '__main__':
---
+++ python_module_03/ex3/ft_achievement_tracker.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3
+
+import random
+
+
+def gen_player_achievements():
+    pass
+
+
+
+
+def main():
+    print('=== Achievement Tracker System ===')
+
+
+if __name__ == '__main__':
+    pass
